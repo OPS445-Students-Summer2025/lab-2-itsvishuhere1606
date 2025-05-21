339fa20f3f2389a82261bfe92e9a9373d2f54a71
Vishesh <vishesh1@myseneca.ca>
2025-05-21 16:04:30 -0400

    Countdown starts from command-line argument using int(sys.argv[1])

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Vishesh
+# Author ID: Vishesh1@myseneca.ca
+# Date Created: 2025/05/21
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
