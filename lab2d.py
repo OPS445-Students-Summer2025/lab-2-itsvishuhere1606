c2c57b973703721f8a241aac8e693170b0866c57
Vishesh <vishesh1@myseneca.ca>
2025-05-21 15:52:52 -0400

    If Statements

---
+++ lab2d.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+#colour = input("Type in a colour and press enter: ")
+#print(colour)
+#print('The colour I typed in is: ' + colour)
+
+import sys
+
+if len(sys.argv) != 3:
+    print(f'Usage: {sys.argv[0]} name age')
+    sys.exit()
+
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+print(f"Hi {name}, you are {age} years old.")
+
