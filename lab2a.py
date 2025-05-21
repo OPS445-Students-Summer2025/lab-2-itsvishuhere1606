bc8ccd279a0bb56886b58f7f5c6cf935fbf0ff75
Vishesh <vishesh1@myseneca.ca>
2025-05-21 16:17:30 -0400

    lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -2,7 +2,4 @@
 
 name = 'Jon'
 age = 20
-#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
-colour = input("Type in a colour and press enter: ")
-#print(colour)
-print('The colour I typed in is: ' + colour)
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
